90ac89dd88435680618007c98eae684b1d4c78b1
nawodyaishan <nawodyain@gmail.com>
2023-02-04 23:41:41 +0530

    Add SignIn Component and Google Auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import NavBar from "./components/NavBar";
+import {auth} from "./firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+
 
 const style = {
     appContainer: 'max-w-[728px] mx-auto test-center',
     sectionContainer: 'flex flex-col  h-[90h] bg-gray-100 mt-10 shadow-xl border relative',
-
 }
 
+
 function App() {
+    // Getting Users in to array
+    const [user] = useAuthState(auth)
+    // console.log(user)
+
     return (<div className={style.appContainer}>
         <section className={style.sectionContainer}>
             {<NavBar/>}
