d1c6b5a9417d47cef6576652f366eca7433e47de
nawodyaishan <nawodyain@gmail.com>
2023-02-05 12:42:34 +0530

    Add message swap on uid

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,3 +1,5 @@
+import {auth} from "../firebase";
+
 const style = {
     message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
     name: `absolute mt-[-4rem] text-gray-600 text-xs`,
@@ -5,10 +7,12 @@ const style = {
     received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
 };
 const Message = ({message}) => {
+    const messageClass = message.uid === auth.currentUser.uid ? `${style.sent}` : `${style.received}`
+
     return (<div>
-        <div className={style.message}>
-            {message ? (<p className={style.name}>Dave</p>) : <div></div>}
-            <p>{message}</p>
+        <div className={`${style.message} ${messageClass}`}>
+            <p className={style.name}>{message.name}</p>
+            <p>{message.text}</p>
         </div>
     </div>);
 };
