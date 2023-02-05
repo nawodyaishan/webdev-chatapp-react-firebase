30ab6fabcd89681aabbb41ef518bfc3854f01a4d
nawodyaishan <nawodyain@gmail.com>
2023-02-05 13:10:26 +0530

    Fix SendMessage position bug

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -4,7 +4,7 @@ import {collection, addDoc, serverTimestamp} from "firebase/firestore";
 
 
 const style = {
-    form: `h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0`,
+    form: `h-14 w-full max-w-[728px]  flex text-xl bottom`,
     input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
     button: `w-[20%] bg-green-500`,
 };
